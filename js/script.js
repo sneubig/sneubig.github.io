737b09868f69b8fd8e3b0a7f47227c7b6a02322d
sneubig <neubig.stephen@gmail.com>
2014-12-05 11:41:33 -0500

    stopgap added to section2 slider

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@
 	$("button").on('click', function() {
 	  $(this).siblings('.read-more').slideToggle( "slow" );
 
-	  if ( $( ".read-more" ).is( "none" ) ) {
-   		$('.read-more').hide();
-  	} else {
-  		$('#section2.container').css({'height': '+=220%'});
-	  	$('#section2').css({'height': '+=220%'});
-  	}
+	//   if ( $( ".read-more" ).is( "none" ) ) {
+	//   	$('#section2.container').css({'max-height': '140%'});
+	//   	$('#section2').css({'max-height': '140%'});
+	//   	$('#section2.container').css({'min-height': '140%'});
+	//   	$('#section2').css({'min-height': '140%'});
+ //  	} else {
+ //  		$('#section2.container').css({'min-height': '240%'});
+	//   	$('#section2').css({'min-height': '240%'});
+ //  	}
 	  
-	  // $('#section2.container').css({'height': '+=475%'}).slideToggle( "slow" );
-	  // $('#section2').css({'height': '+=475%'}).slideToggle( "slow" );
 	});
 
 
