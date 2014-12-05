01108e407b25a57b56d8d4c935b9694477e4a072
sneubig <neubig.stephen@gmail.com>
2014-12-05 10:26:35 -0500

    update section2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 
 	$("button").on('click', function() {
 	  $(this).siblings('.read-more').slideToggle( "slow" );
+
+	  // $('#section2')
+	  // $('#section2.container').css({'height': '+=475%'});
+	  // $('#section2').css({'height': '+=475%'});
 	});
 
 
