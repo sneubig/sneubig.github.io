8a41c839a98f263b1ccb2a6223c98a3cc451dc63
sneubig <neubig.stephen@gmail.com>
2014-12-05 10:45:51 -0500

    still error with sliding section

--- js/script.js
+++ js/script.js
@@ -2,9 +2,15 @@
 	$("button").on('click', function() {
 	  $(this).siblings('.read-more').slideToggle( "slow" );
 
-	  // $('#section2')
-	  // $('#section2.container').css({'height': '+=475%'});
-	  // $('#section2').css({'height': '+=475%'});
+	  if ( $( ".read-more" ).is( "none" ) ) {
+   		$('.read-more').hide();
+  	} else {
+  		$('#section2.container').css({'height': '+=220%'});
+	  	$('#section2').css({'height': '+=220%'});
+  	}
+	  
+	  // $('#section2.container').css({'height': '+=475%'}).slideToggle( "slow" );
+	  // $('#section2').css({'height': '+=475%'}).slideToggle( "slow" );
 	});
 
 
