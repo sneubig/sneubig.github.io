8596eb31023da5b32843adaad8d77551ca2592f1
sneubig <neubig.stephen@gmail.com>
2015-01-18 13:56:33 -0500

    added headroom.js and added hiding nav bar

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ $(function(){
 	   $(".nav").find(".active").removeClass("active");
 	   $(this).parent().addClass("active");
 	});
+
+	// grab an element
+	var myElement = document.querySelector("nav");
+	// construct an instance of Headroom, passing the element
+	var headroom  = new Headroom(myElement);
+	// initialise
+	headroom.init(); 
 });
 
 
